64c898f73b9ea93ff2b56a89b2b67365c36d666c
Luvianto <126235306+Luvianto@users.noreply.github.com>
2024-03-09 14:43:26 +0700

    Added Player Prefs

--- Assets/Scripts/SliderSetting.cs
+++ Assets/Scripts/SliderSetting.cs
@@ -6,12 +6,32 @@ using UnityEngine.Audio;
 
 public class SliderSetting : MonoBehaviour
 {
-    [SerializeField] private AudioMixer musicMixer;
-    [SerializeField] private Slider musicSlider;
+    [SerializeField] private AudioMixer Mixer;
+    [SerializeField] private Slider musicSlider, sfxSlider;
 
+    private void Start()
+    {
+        loadMusic();
+    }
     public void SetMusic()
     {
         float volume = musicSlider.value;
-        musicMixer.SetFloat("music",Mathf.Log10(volume)*20);
+        Mixer.SetFloat("music",Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+    }
+
+    public void SetSfx()
+    {
+        float volume = sfxSlider.value;
+        Mixer.SetFloat("sfx", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("sfxVolume", volume);
+    }
+
+    public void loadMusic()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        SetMusic();
+        sfxSlider.value = PlayerPrefs.GetFloat("sfxVolume");
+        SetSfx();
     }
 }
